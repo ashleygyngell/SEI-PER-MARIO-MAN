e0f0c51e5f8a25e4ca8d93ff8491ba804cbfaee9
Ashley Gyngell <ashleygyngell@gmail.com>
2022-02-18 17:13:40 +0000

    Grid safely generated

---
+++ scripts.js/app.js
@@ -0,0 +1,67 @@
+// Dom Elements
+const grid = document.querySelector('.game-grid');
+const cells = [];
+
+// Grid Variables
+const gameGridWidth = 18;
+const cellCount = gameGridWidth * gameGridWidth;
+const penWalls = [134, 135, 152, 153];
+const walls = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+  //
+  18, 26, 27, 35,
+  //
+  36, 38, 39, 40, 41, 42, 44, 45, 47, 48, 49, 50, 51, 53,
+  //
+  54, 56, 57, 58, 59, 60, 62, 63, 65, 66, 67, 68, 69, 71,
+  //
+  72, 89,
+  //
+  90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 107,
+  //
+  108, 114, 119, 125,
+  //
+  126, 127, 128, 129, 130, 132, 137, 139, 140, 141, 142, 143,
+  //
+  144, 161,
+  //
+  162, 163, 164, 165, 166, 168, 173, 175, 176, 177, 178, 179,
+  //
+  180, 181, 182, 183, 184, 186, 187, 188, 189, 190, 191, 193, 194, 195, 196,
+  197,
+  //
+  198, 206, 207, 215,
+  //
+  216, 218, 219, 220, 222, 224, 225, 227, 229, 230, 231, 233,
+  //
+  234, 236, 237, 238, 240, 245, 247, 248, 249, 251,
+  //
+  252, 258, 259, 260, 261, 262, 263, 269,
+  //
+  270, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285,
+  287,
+  //
+  288, 305,
+  //
+  306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320,
+  321, 322, 323,
+];
+
+const pacManPosition = 149;
+
+// functions
+function createGameGrid() {
+  for (let i = 0; i < cellCount; i++) {
+    const cell = document.createElement('div');
+    cell.setAttribute('data-id', i);
+    if (penWalls.includes(i)) cell.classList.add('pen-wall');
+    if (walls.includes(i)) cell.classList.add('wall');
+    cell.textContent = i;
+    cells.push(cell);
+    grid.appendChild(cell);
+  }
+}
+
+// here i need to generate pikichu, and the ghost
+
+createGameGrid();
