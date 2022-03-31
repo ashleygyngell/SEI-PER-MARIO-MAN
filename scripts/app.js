acd11ae5232f8d486c7587e1d4ef65d6c5de514a
Ashley Gyngell <ashleygyngell@gmail.com>
2022-03-31 20:23:44 +0100

    fix refresh

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 // Dom Elements
 const startScreen = document.querySelector('.start-screen');
 const gameoverScreen = document.querySelector('.gameover-screen');
+const youwin = document.querySelector('.you-win');
 const scoreDisplay = document.querySelector('.score-grid');
 const scoreDisplayEnd = document.querySelector('.score-grid-end');
 const startButton = document.querySelector('#start');
@@ -443,6 +444,7 @@ function gameStart() {
   document.addEventListener('keydown', handleKeyDown);
   setInterval(startGhostHunt, 400);
   audio.game.play();
+
   // setInterval(gameStatusCheck(), 200);
   // setInterval(startGhost2Hunt, 500);
 }
